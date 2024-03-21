d84b87d5399250ba166b6d57a44184fdde7ec62b
ramcharan2410 <ramcharanvakkalanka@gmail.com>
2024-03-21 21:46:03 +0530

    updated styling of Login and Signup pages

--- todo-frontend/src/components/Login.js
+++ todo-frontend/src/components/Login.js
@@ -39,51 +39,57 @@ const Login = ({ email, setEmail, setToken }) => {
   }
   return (
     <div className="login">
-      <div className="login-header">Login Header</div>
-      <div className="login-title">Todo</div>
-      <div className="login-form">
-        <form onSubmit={(e) => handleLoginSubmit(e)}>
-          <label htmlFor="login-email">email:</label>
-          <input
-            type="text"
-            value={email}
-            id="login-email"
-            name="login-email"
-            placeholder="Enter your email"
-            required
-            onChange={(e) => {
-              setEmail(e.target.value)
-              setInvalidCredentials(false)
-            }}
-          />
-          <br />
-          <label htmlFor="login-password">password:</label>
-          <input
-            type="password"
-            value={password}
-            id="login-password"
-            name="login-password"
-            placeholder="Enter your password"
-            onChange={(e) => {
-              setPassword(e.target.value)
-              setInvalidCredentials(false)
-            }}
-          />
-          <br />
-          <p
-            className="user-not-found"
-            style={{ display: invalidCredentials ? 'block' : 'none' }}
-          >
-            Invalid Credentials
-          </p>
-          <button type="submit" className="login-button">
-            login
-          </button>
+      <div className="login-header">Terabh Intelligence</div>
+      <div className="login-title-container">
+        <div className="login-title">Todo</div>
+      </div>
+      <div className="login-form-container">
+        <div className="login-form">
+          <form onSubmit={(e) => handleLoginSubmit(e)}>
+            <label htmlFor="login-email">email:</label>
+            <input
+              type="text"
+              value={email}
+              id="login-email"
+              name="login-email"
+              placeholder="Enter your email"
+              required
+              onChange={(e) => {
+                setEmail(e.target.value)
+                setInvalidCredentials(false)
+              }}
+            />
+            <br />
+            <label htmlFor="login-password">password:</label>
+            <input
+              type="password"
+              value={password}
+              id="login-password"
+              name="login-password"
+              placeholder="Enter your password"
+              onChange={(e) => {
+                setPassword(e.target.value)
+                setInvalidCredentials(false)
+              }}
+            />
+            <br />
+            <p
+              className="errorLabel"
+              style={{ display: invalidCredentials ? 'block' : 'none' }}
+            >
+              Invalid Credentials
+            </p>
+          </form>
+          <div className="login-button-container">
+            <button type="submit" className="login-button">
+              login
+            </button>
+          </div>
           <div className="signup-instead">
             new user?
             <a href="/signup">sign up</a>
           </div>
-        </form>
+        </div>
       </div>
       <div className="login-footer">
         Terabh Intelligence-Copyright &copy; {getYear()}
--- todo-frontend/src/components/Signup.js
+++ todo-frontend/src/components/Signup.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Signup = ({
-  userName,
-  setUserName,
-  email,
-  setEmail,
-  setToken,
-}) => {
+const Signup = ({ userName, setUserName, email, setEmail, setToken }) => {
   const [userExists, setUserExists] = useState(false)
   const [password, setPassword] = useState('')
   const [emailError, setEmailError] = useState('')
@@ -54,81 +48,88 @@ const Signup = ({
   }
   return (
     <div className="signup">
-      <div className="signup-header">Something</div>
-      <div className="title">Todo</div>
-      <div className="signup-form">
-        <form onSubmit={(e) => handleSignupSubmit(e)}>
-          <label htmlFor="signup-email">Email:</label>
-          <input
-            type="text"
-            value={email}
-            id="signup-email"
-            name="signup-email"
-            placeholder="Enter your email"
-            required
-            onChange={(e) => {
-              setEmail(e.target.value)
-              if (!emailRegex.test(e.target.value)) {
-                setEmailError('Enter a valid email')
-              } else {
-                setEmailError('')
-              }
-              setUserExists(false)
-            }}
-          />
-          <label
-            className="errorLabel"
-            style={{
-              display: emailError ? 'block' : 'none',
-            }}
-          >
-            {emailError}
-          </label>
-          <br />
-          <label htmlFor="signup-password">Password:</label>
-          <input
-            type="password"
-            value={password}
-            id="signup-password"
-            name="signup-password"
-            placeholder="Enter your password"
-            onChange={(e) => {
-              setPassword(e.target.value)
-              if (e.target.value.length === 0) {
-                setPasswordError('Please enter a password')
-              } else if (e.target.value.length < 8) {
-                setPasswordError('The password must be 8 characters or longer')
-              } else {
-                setPasswordError('')
-              }
-            }}
-          />
-          <label
-            className="errorLabel"
-            style={{
-              display: passwordError ? 'block' : 'none',
-            }}
-          >
-            {passwordError}
-          </label>
-          <br />
-          <button type="submit" className="signup-button">
-            Signup
-          </button>
-          <br/>
-          <label
-            className="errorLabel"
-            style={{
-              display: userExists ? 'block' : 'none',
-            }}
-          >
-            User already registered
-          </label>
-          <div className="login-instead">
-            Already an existing user?
-            <a href="/login">Login</a>
+      <div className="signup-header">Terabh Intelligence</div>
+      <div className="signup-title-container">
+        <div className="signup-title">Todo</div>
+      </div>
+      <div className="signup-form-container">
+        <div className="signup-form">
+          <form onSubmit={(e) => handleSignupSubmit(e)}>
+            <label htmlFor="signup-email">email:</label>
+            <input
+              type="text"
+              value={email}
+              id="signup-email"
+              name="signup-email"
+              placeholder="Enter your email"
+              required
+              onChange={(e) => {
+                setEmail(e.target.value)
+                if (!emailRegex.test(e.target.value)) {
+                  setEmailError('Enter a valid email')
+                } else {
+                  setEmailError('')
+                }
+                setUserExists(false)
+              }}
+            />
+            <label
+              className="errorLabel"
+              style={{
+                display: emailError ? 'block' : 'none',
+              }}
+            >
+              {emailError}
+            </label>
+            <br />
+            <label htmlFor="signup-password">password:</label>
+            <input
+              type="password"
+              value={password}
+              id="signup-password"
+              name="signup-password"
+              placeholder="Enter your password"
+              onChange={(e) => {
+                setPassword(e.target.value)
+                if (e.target.value.length === 0) {
+                  setPasswordError('Please enter a password')
+                } else if (e.target.value.length < 8) {
+                  setPasswordError(
+                    'The password must be 8 characters or longer'
+                  )
+                } else {
+                  setPasswordError('')
+                }
+              }}
+            />
+            <p
+              className="errorLabel"
+              style={{
+                display: passwordError ? 'block' : 'none',
+              }}
+            >
+              {passwordError}
+            </p>
+            <br />
+            <p
+              className="errorLabel"
+              style={{
+                display: userExists ? 'block' : 'none',
+              }}
+            >
+              user already registered
+            </p>
+          </form>
+          <div className="signup-button-container">
+            <button type="submit" className="signup-button">
+              signup
+            </button>
           </div>
-        </form>
+          <div className="login-instead">
+            already an existing user?
+            <a href="/login">login</a>
+          </div>
+        </div>
       </div>
       <div className="signup-footer">
         Terabh Intelligence-Copyright &copy; {getYear()}
